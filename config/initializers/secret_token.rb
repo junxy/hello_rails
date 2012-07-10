771057dc1292b9a2002b6901ac347713f0bc0a54
xu yijun <junxy@xumatoMacBook-Pro.local>
2012-07-10 12:15:30 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloRails::Application.config.secret_token = 'e3a66d004a5e6271df0afadb809625d7f5d867d28200c5e9b1724f6327633da6911793ae76125530ceea5d1d34a63aea32a51061bd2e786d28e59f3bec650413'
